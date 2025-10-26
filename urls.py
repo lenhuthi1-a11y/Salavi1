b24dea44ec50c1394b9189c8d7540e4ffd98368b
lenhuthi1-a11y <lenhuthi1@gmail.com>
2025-10-26 14:59:59 +0700

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,10 +1,7 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('nhapkho/', views.nhap_kho_view, name='nhapkho'),
-    path('xoa-phieu/<int:id>/', views.xoa_phieu, name='xoa_phieu'),
-    path('chi-tiet-phieu-nhap/<int:id>/', views.chi_tiet_phieu_nhap_view, name='chi_tiet_phieu_nhap'),
-    path('tao-phieu-nhap/', views.tao_phieu_nhap_view, name='tao_phieu_nhap'),
-
+    path('admin/', admin.site.urls),
+    path('', include('Chucnang.urls')),
 ]
